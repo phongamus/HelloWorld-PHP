8ed3f463acb9c8d24ab016058fb47e647236a40e
phongamus <phle03@gmail.com>
2020-04-19 10:48:27 -0700

    added json message validation

--- awsTokenHandler.py
+++ awsTokenHandler.py
@@ -1,16 +1,25 @@
 import json
 import boto3
 
+checklist = [
+    'refreshTokenKey', 
+    'QBORealmID', 
+    'x_refresh_token_expires_in'
+]
+
+
 def lambda_handler(event, context):
     # TODO implement
     print(event)
     
-    jsonPayload = json.loads(event["body"])
-    print(jsonPayload)
+    jsonStrPayload = event["body"]
+    jsonPayload = json.loads(jsonStrPayload)
+    if(checkValidJson(jsonPayload, checklist)):
+        print('success')
     return event["body"]
 
 
-def postToTable(primaryKey, jsonPayload, tableField, tableName, primaryKeyName):
+def postToTable(primaryKey, jsonStrPayload, tableField, tableName, primaryKeyName):
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table(tableName)
     expression = "set "+tableField+" = :r"
@@ -20,7 +29,7 @@ def postToTable(primaryKey, jsonPayload, tableField, tableName, primaryKeyName):
         },
         UpdateExpression=expression,
         ExpressionAttributeValues={
-            ':r': ticketNum,
+            ':r': jsonStrPayload,
         }
     )
     print('posted the deal and ticketnum to Dynamo')
@@ -28,8 +37,22 @@ def postToTable(primaryKey, jsonPayload, tableField, tableName, primaryKeyName):
 def parseJson(jsonObj):
     pass
 
+def checkValidJson(messageJsonAsDict, checklist):
+    notFoundItems = 0
+    for item in checklist:
+        if ( item not in messageJsonAsDict):
+            print(item + " not found")
+            notFoundItems +=1
+    if notFoundItems > 0:
+        print ( 'items not found: ' + str(notFoundItems))
+        return False
+    else:
+        print('All items Found')
+        return True
 
-if(True):
+
+DEBUG = True
+if(DEBUG):
     with open('samplePostToDb.json') as f:
         awsJsonBody = json.load(f)
     event = {}
