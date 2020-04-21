57ba955d87af999271a9af345681e59787722543
phongamus <phle03@gmail.com>
2020-04-21 12:34:45 -0700

    handling if mid is missing

--- postToDb.php
+++ postToDb.php
@@ -3,17 +3,22 @@
 require_once(__DIR__ . '/vendor/autoload.php');
 use QuickBooksOnline\API\DataService\DataService;
 
-session_start();
 echo '<pre>';
-function postToDb()
+// print_r($_GET['mid']);
+$mid = $_GET['mid'];
+
+session_start();
+
+function postToDb($mid)
 {
     $accessToken = $_SESSION['sessionAccessToken'];
     $auth_server = 'https://fpzxkuf1lj.execute-api.us-east-1.amazonaws.com/';
-    // $auth_server = 'https://enay4layxh36s.x.pipedream.net';
+    // $auth_server = 'https://enb1oanvszux.x.pipedream.net';
     try{
         $client = new GuzzleHttp\Client();
         $res1 = $client->post($auth_server, [
             'json' => [
+                'mid' => $mid,
                 'refreshTokenKey' => $accessToken->getRefreshToken(),
                 'QBORealmID' => $accessToken->getRealmID(),
                 'x_refresh_token_expires_in' => $accessToken->getRefreshTokenExpiresAt()
@@ -39,11 +44,16 @@ function postToDb()
     
 }
 
-$response = postToDb();
+if(isset($_GET['mid']) && $_GET['mid'] != ""){
+    $response = postToDb($mid);
+    echo '</br>';
+    echo($response);
 
-echo '</br>';
-echo($response);
+    echo '</br>Done';
+    echo '</pre>';
+} else{
+    echo 'something is missing';
+}
 
-echo '</br>Done';
-echo '</pre>'
+
 ?>
